e158ada8376ad11b6be14ec42750850f1feea2de
Phil81334 <philgibsongames@outlook.com>
2024-10-28 18:05:56 +0000

    update

--- qt_utility.py
+++ qt_utility.py
@@ -1,19 +1,43 @@
-from PySide6.QtWidgets import QMessageBox
+from PySide6.QtWidgets import QApplication, QMessageBox
 
-def displayMessageBox(message: str) -> None:
-    msg_box = QMessageBox()
-    msg_box.setIcon(QMessageBox.Information)
-    msg_box.setWindowTitle("Information")
-    msg_box.setText(message)
-    msg_box.setStandardButtons(QMessageBox.Ok)
-    msg_box.exec()
+def displayMessageBox(message: str, x: int = None, y: int = None) -> None:
+    msgBox = QMessageBox()
+    msgBox.setIcon(QMessageBox.Information)
+    msgBox.setWindowTitle("Information")
+    msgBox.setText(message)
+    msgBox.setStandardButtons(QMessageBox.Ok)
+    
+    if x is None or y is None:
+        screenWidth, screenHeight = getScreenDimms()
+
+        if x is None:
+            # Get screen geometry and center horizontally
+            msgBox_width = msgBox.sizeHint().width()
+            x = (screenWidth - msgBox_width) // 2
+
+        if y is None:
+            # Get screen geometry and center vertically
+            msgBox_height = msgBox.sizeHint().height()
+            y = (screenHeight - msgBox_height) // 2  # Vertically center
+    
+    msgBox.move(x, y)  # Set position
+    msgBox.exec()
 
 def displayYesNoMessageBox(message: str) -> bool:
-    msg_box = QMessageBox()
-    msg_box.setIcon(QMessageBox.Question)
-    msg_box.setWindowTitle("Confirmation")
-    msg_box.setText(message)
-    msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-    msg_box.setDefaultButton(QMessageBox.No)
-    result = msg_box.exec()
+    msgBox = QMessageBox()
+    msgBox.setIcon(QMessageBox.Question)
+    msgBox.setWindowTitle("Confirmation")
+    msgBox.setText(message)
+    msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+    msgBox.setDefaultButton(QMessageBox.No)
+    result = msgBox.exec()
     return result == QMessageBox.Yes
+
+def getScreenDimms() -> tuple[int, int]:
+    screen = QApplication.primaryScreen()
+    screenGeometry = screen.geometry()
+
+    screenWidth = screenGeometry.width()
+    screenHeight = screenGeometry.height()
+
+    return screenWidth, screenHeight
